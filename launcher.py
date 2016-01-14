cdc8e40ad04e49eb0c76182366f241a91e0a5428
Moggieggwp <moggieggwp@mail.ru>
2016-01-15 00:25:39 +0200

    Create launcher.py

---
+++ launcher.py
@@ -0,0 +1,13 @@
+import idgenerate
+import size
+import magicnum
+import typegenerate
+import direction
+import tag
+
+idgenerate.generate_save_id()
+size.generate_size()
+magicnum.generate_magic_num()
+typegenerate.gener_type()
+direction.generate_direction()
+tag.generate_tag()
