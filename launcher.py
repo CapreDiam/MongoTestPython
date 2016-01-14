837f15773162596a0f7dfd97408fd071a67edcb5
Moggieggwp <moggieggwp@mail.ru>
2016-01-15 01:06:30 +0200

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -4,6 +4,9 @@ import magicnum
 import typegenerate
 import direction
 import tag
+import tag_length
+import comment
+import comment_length
 
 idgenerate.generate_save_id()
 size.generate_size()
@@ -11,3 +14,6 @@ magicnum.generate_magic_num()
 typegenerate.gener_type()
 direction.generate_direction()
 tag.generate_tag()
+comment.generate_comment()
+comment_length.generate_comment_length()
+tag_length.generate_tag_length()
