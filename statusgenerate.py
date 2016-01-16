6ec63f95342ac1cbcb554d72e397f48f091daad0
Moggieggwp <moggieggwp@mail.ru>
2016-01-16 15:12:25 +0200

    Update statusgenerate.py

--- statusgenerate.py
+++ statusgenerate.py
@@ -2,18 +2,22 @@ import random
 def status_gen():
 	file=open('request.txt')
 	ids=file.readlines()
+	file.close()
 	file=open('datetime.txt')
 	datetime=file.readlines()
 	file.close()	
 	status = [["New","To Provider","Partially Filled","Filled"],["New", "To Provider", "Filled"],["New","Filled"],["New","Partially Filled","To Provider","Filled"],["New","Partially Filled","To Provider","Rejected","Filled"],["To Provider"],["To Provider","Rejected"],["To Provider","Filled"],["New","To Provider"],["New","To Provider","Rejected"],["New","Partially Filled"],["New"],["New","To Provider","Partially Filled"]]
 	i=0
+	j=0
 	file=open('request_final.txt','w')
+	
 	while i<3000:
-		res=ids[i]
-		dt=datetime[i]
+		res=ids[i]		
 		a=random.randint(0,12)
 		b=len(status[a])
-		for x in range(b):		
+		for x in range(b):
+			dt=datetime[j]		
 			file.write(str(res[0:len(res)-1]) + " date: new Date(" +str(dt[0:len(dt)-1])+'), ' + "status: "+ '"'+status[a][x]+'"'+'}'+' )'+'\n')
+			j=j+1
 		i=i+1
 	file.close()	
