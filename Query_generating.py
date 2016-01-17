ceea478d5d333d4ae94a6f9c6bd81471ea8d24bc
Dima <moggieggwp@mail.ru>
2016-01-17 17:19:04 +0200

    Update Query_generating.py

--- Query_generating.py
+++ Query_generating.py
@@ -2,6 +2,11 @@ import subprocess
 import commands
 
 def insert_query():
+	operation ="mongo < request_final.txt"
+	res=commands.getoutput(operation)
+	file = open('log','w')
+	file.write(res)
+	file.close()
         statuses = ["New", "To Provider", "Partially Filled","Filled","Rejected"]
         count=len(statuses)
         i=0
@@ -11,9 +16,8 @@ def insert_query():
                 file.write(quer1+'\n')
         file.write( "db.orders.aggregate( [ { $match : { provider: "+'"'+'*'+'"'+" } }, { $group: { _id: "+'"'+"$provider"+'"'+", sum: { $sum: "+'"'+"$price"+'"'+" } } } ] )"+'\n')#Count sum by "*" provider
         file.write( "db.orders.aggregate( [ { $match : { provider: "+'"'+'~'+'"'+" } }, { $group: { _id: "+'"'+"$provider"+'"'+", sum: { $sum: "+'"'+"$price"+'"'+" } } } ] )"+'\n')#Count sum by "~" provider
-	file.write("db.orders.aggregate( { $group: { _id: "+'"'+"$id"+'"'+", count: { $sum: 1 } } }, { $sort: { _id: 1 } }, { $limit: 3 } )"+'\n')#Count id by every status(only for 3 orders) 
+	file.write("db.orders.aggregate( { $group: { _id: "+'"'+"$id"+'"'+", count: { $sum: 1 } } }, { $limit: 3 } )"+'\n')#Count id by every status(only for 3 orders) 
 	file.write("db.orders.aggregate( [ { $match : { date: { $gte: new Date("+'"'+"2016"+'\\'+"1"+'\\'+"15 9:12:24.25"+'"'+"),$lte: new Date("+'"'+"2016"+'\\'+"1"+'\\'+"15 9:12:24.725"+'"'+ ") } } }, { $group: { _id: null, count: { $sum: 1 } } } ] )"+'\n')#Count orders between 2 dates
-
         file.close()
         
 
@@ -22,7 +26,7 @@ def write_reslut():
        	buff=file.readlines()
      	i=0
     	f = open ('Test/results','w')
-	operation = "mongo < querys"
+	operation = "mongo < Test/querys"
 	result = commands.getoutput(operation)
 	f.write(result[50:len(result)-3])   
 	f.close()     
