6532b2edc5776b9523b9cf022f3b265d1e974636
CapreDiam <ig.bukin2022@gmail.com>
2016-01-18 11:07:43 +0200

    Update generateinsertrequest.py

--- generateinsertrequest.py
+++ generateinsertrequest.py
@@ -18,62 +18,79 @@ box=''
 def generate_request():
 	i = 0
 	request_file=  open('request.txt', 'w')
+	json_data_file=open('jsondatafile.txt', 'w')
 	while i < 3000:
-		DBINSERT='db.orders.insert( {  provider: "'	
+		DBINSERT='db.orders.insert( {  provider:"'
+		JSONINSERT='{"provider": "'	
 		if i < 1499 :	
 			#FXOpen
 			DBINSERT=DBINSERT + '~", '
-			DBINSERT=DBINSERT+'id' +': "'+ open_file(ID,i) +'", '
+			JSONINSERT=JSONINSERT+ '~", '
+			DBINSERT=DBINSERT+'id' +': "'+ open_file(ID,i) +'",'
+			JSONINSERT=JSONINSERT+ '"id"' +': "'+ open_file(ID,i) +'",'
 			#concatenate_request("id",box,DBINSERT)
 			
-			DBINSERT=DBINSERT+'type' +': "'+ open_file(TYPE,i) +'", '
+			DBINSERT=DBINSERT+'type' +': "'+ open_file(TYPE,i) +'",'
+			JSONINSERT=JSONINSERT+ '"type"' +': "'+ open_file(TYPE,i) +'",'
 			#concatenate_request("type",box,DBINSERT)
 				
-			DBINSERT=DBINSERT+'price: ' + str(open_file(PRICE,i)) +', '
+			DBINSERT=DBINSERT+'price: ' + str(open_file(PRICE,i)) +','
+			JSONINSERT=JSONINSERT+ '"price"' +': "'+ open_file(PRICE,i) +'",'
 			#concatenate_request("price",box,DBINSERT)
 			
-			DBINSERT=DBINSERT+'direction' +': "'+ open_file(DIRECTION,i) +'", '
+			DBINSERT=DBINSERT+'direction' +': "'+ open_file(DIRECTION,i) +'",'
+			JSONINSERT=JSONINSERT+ '"direction"' +': "'+ open_file(DIRECTION,i) +'",'
 			#concatenate_request("direction",box,DBINSERT)
 			
-			DBINSERT=DBINSERT+'currency' +': "'+ open_file(CURRENCY,i) +'", '
+			DBINSERT=DBINSERT+'currency' +': "'+ open_file(CURRENCY,i) +'",'
+			JSONINSERT=JSONINSERT+ '"currency"' +': "'+ open_file(CURRENCY,i) +'",'
 			#concatenate_request("currency",box,DBINSERT)	
 			
-			DBINSERT=DBINSERT+'duration' +': "'+ open_file(DURATION,i) +'", '
-			#concatenate_request("duration",box,DBINSERT) 
+			DBINSERT=DBINSERT+'duration' +': "'+ open_file(DURATION,i) +'",'
+			JSONINSERT=JSONINSERT+ '"duration"' +': "'+ open_file(DURATION,i) +'",'
+			#concatenate_request("duration",box,DBINSERT)
 			
-			DBINSERT=DBINSERT+'comment_length' +': "'+ open_file(COMMENTLEN,i) +'", '
+			DBINSERT=DBINSERT+'comment_length' +': "'+ open_file(COMMENTLEN,i) +'",'
+			JSONINSERT=JSONINSERT+ '"comment_length"' +': "'+ open_file(COMMENTLEN,i) +'",'
 			#concatenate_request("comment_length",box,DBINSERT)
 			
-			DBINSERT=DBINSERT+'comment' +': "'+ open_file(COMMENT,i) +'", '
+			DBINSERT=DBINSERT+'comment' +': "'+ open_file(COMMENT,i) +'",'
+			JSONINSERT=JSONINSERT+ '"comment"' +': "'+ open_file(COMMENT,i) +'",'
 			#concatenate_request("comment",box,DBINSERT)
 			
-			DBINSERT=DBINSERT+'tag_length' +': "'+ open_file(TAGLEN,i) +'", '
+			DBINSERT=DBINSERT+'tag_length' +': "'+ open_file(TAGLEN,i) +'",'
+			JSONINSERT=JSONINSERT+ '"tag_length"' +': "'+ open_file(TAGLEN,i) +'",'
 			#concatenate_request("tag_length",box,DBINSERT)
 			
-			DBINSERT=DBINSERT+'tag' +': "'+ open_file(TAG,i) +'", '
+			DBINSERT=DBINSERT+'tag' +': "'+ open_file(TAG,i) +'",'
+			JSONINSERT=JSONINSERT+ '"tag"' +': "'+ open_file(TAG,i) +'",'
 			#concatenate_request("tag",box,DBINSERT)	
 		else:
 			#FXCM
 			DBINSERT=DBINSERT + '*", '
-			
-			DBINSERT=DBINSERT+'id' +': "'+ open_file(ID,i) +'", '
+			JSONINSERT=JSONINSERT+ '*", '
+			DBINSERT=DBINSERT+'id' +': "'+ open_file(ID,i) +'",'
+			JSONINSERT=JSONINSERT+ '"id"' +': "'+ open_file(ID,i) +'",'
 			#concatenate_request("id",box,DBINSERT)
 			
-			DBINSERT=DBINSERT+'type' +': "'+ open_file(TYPE,i) +'", '
+			DBINSERT=DBINSERT+'type' +': "'+ open_file(TYPE,i) +'",'
+			JSONINSERT=JSONINSERT+ '"type"' +': "'+ open_file(TYPE,i) +'",'
 			#concatenate_request("type",box,DBINSERT)
 				
-			DBINSERT=DBINSERT+'price: ' + str(open_file(PRICE,i)) +', '
+			DBINSERT=DBINSERT+'price: ' + str(open_file(PRICE,i)) +','
+			JSONINSERT=JSONINSERT+ '"price"' +': "'+ open_file(PRICE,i) +'",'
 			#concatenate_request("price",box,DBINSERT)
 			
-			DBINSERT=DBINSERT+'direction' +': "'+ open_file(DIRECTION,i) +'", '
+			DBINSERT=DBINSERT+'direction' +': "'+ open_file(DIRECTION,i) +'",'
+			JSONINSERT=JSONINSERT+ '"direction"' +': "'+ open_file(DIRECTION,i) +'",'
 			#concatenate_request("direction",box,DBINSERT)
 			
-			DBINSERT=DBINSERT+'currency' +': "'+ open_file(CURRENCY,i) +'", '
+			DBINSERT=DBINSERT+'currency' +': "'+ open_file(CURRENCY,i) +'",'
+			JSONINSERT=JSONINSERT+ '"currency"' +': "'+ open_file(CURRENCY,i) +'",'
 			#concatenate_request("currency",box,DBINSERT)
-			DBINSERT=DBINSERT+'desciption' + ': "desciption", '
-			#concatenate_request("desciption","desciption",DBINSERT)
 		i = i + 1	
 		request_file.write(DBINSERT+'\n')
+		json_data_file.write(JSONINSERT+'\n')
 	request_file.close()		
 		
 def open_file(name,i):
@@ -88,8 +105,9 @@ def open_file(name,i):
 	return box 	
 def concatenate_request(name_column,text,dbinsert): 
 	global DBINSERT
-	DBINSERT=DBINSERT+name_column +": "+ text +", "
+	DBINSERT=DBINSERT+name_column +": "+ text +","
 
 def price(price,dbinsert):
 	global DBINSERT
-	DBINSERT=DBINSERT+"price: " + str(price) +", "
+	DBINSERT=DBINSERT+"price: " + str(price) +","
+		
