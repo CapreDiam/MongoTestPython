dd086067bc2def59ce1d95ca9cd672a732bf76d0
CapreDiam <ig.bukin2022@gmail.com>
2016-01-18 11:09:30 +0200

    Update size.py

--- size.py
+++ size.py
@@ -1,9 +1,10 @@
 import random
+
 def generate_size():
 	i=0
 	file = open('size.txt', 'w')
 	while i < 3000:
-		size=str(random.randint(1000, 9999)+random.random())
+		size=str(random.randint(100000, 999999)+random.random())
 		file.write(size + '\n')
 		i = i + 1
 	file.close()	
