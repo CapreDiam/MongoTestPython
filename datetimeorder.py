2d2e2db5fc30de768f81e5eae49b61177fd23e6a
CapreDiam <ig.bukin2022@gmail.com>
2016-01-19 23:33:24 +0200

    Update datetimeorder.py

--- datetimeorder.py
+++ datetimeorder.py
@@ -1,29 +1,35 @@
+def checkHMS(HMSchek, HMSincr):
+	if((HMSchek[0]+1) == 60):
+		HMSchek[0] = 0
+		HMSincr[0] = HMSincr[0] + 1
+
 def date_time():
 	i=0
 	year=2016
 	month=1
 	day=15
-	hours=9
-	minute=12
-	second=24
-	millisecond=1
+	timeArray=[9,12,24]
+	hours=[9]
+	minute=[12]
+	second=[24]
+	millisecond=[1]
 	file = open('datetime.txt', 'w')
 	while i < 9999:
-		file.write('"' + str(year) + '\\' + str(month) + '\\' + str(day) +' '+ str(hours) + ':' + str(minute) + ':' + str(second) + '.' + str(millisecond)+'"' +'\n')
+		file.write('"' + str(year) + '\\' + str(month) + '\\' + str(day) +\
+		' '+ str(hours[0]) + ':' + str(minute[0]) + ':' + str(second[0]) +\
+		'.' + str(millisecond[0])+'"' +'\n')
 		i = i + 1
-		if((millisecond+10)>999):
-			millisecond=1
-			second = second + 1
-		if((second)==60):
+		if((millisecond[0]+10)>999):
+			millisecond[0]=1
+			second[0] = second[0] + 1
+		'''if((second)==60):
 			second=0
 			minute = minute + 1
 		if((minute)==60):
 			minute=0
-			hours = hours + 1
-		millisecond = millisecond + 4
+			hours = hours + 1'''
+		millisecond[0] = millisecond[0] + 4
+		checkHMS(second,minute)
+		checkHMS(second,minute)
 	file.close()	
 
-def chekHMS(HMSchek, HMSincr):
-	if((HMSchek+1) == 60):
-		HMSchek = 0
-		HMSincr = HMSincr + 1
