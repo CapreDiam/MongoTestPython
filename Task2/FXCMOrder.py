7efc67af6ddcee4fe434e2886d8156126dbb791d
CapreDiam <ig.bukin2022@gmail.com>
2016-01-21 15:57:26 +0200

    Update FXCMOrder.py

--- Task2/FXCMOrder.py
+++ Task2/FXCMOrder.py
@@ -1,1 +1,10 @@
-
+class FXCMOrder(Orders):
+	
+	def __init__(self):
+		return [provider(),decsription()]		
+		
+	def provider(self):
+		return '*'
+	
+	def decsription(self):
+		return 'description'
