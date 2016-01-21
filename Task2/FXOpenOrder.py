48f86c3223ea8d299cd906421914e5d8024ac1c5
CapreDiam <ig.bukin2022@gmail.com>
2016-01-21 18:12:06 +0200

    Update FXOpenOrder.py

--- Task2/FXOpenOrder.py
+++ Task2/FXOpenOrder.py
@@ -1,14 +1,13 @@
 import string,random
 
 class FXOpenOrder:
-durations = ['Immediate or cancel', 'Good Till Cancel']
 	
 	def __init__(self):
-		return [provider(),duration(),comment_length(),comment(),tag_length(),tag(),magicalNumber()]	
+		self.durations = ['Immediate or cancel', 'Good Till Cancel']	
 	def provider(self):
 		return '*'
 	def duration(self):
-		return durations[random.randint(0,1)]
+		return self.durations[random.randint(0,1)]
 	def comment_length(self):
 		return 30
 	
@@ -16,13 +15,13 @@ durations = ['Immediate or cancel', 'Good Till Cancel']
     		return ''.join([random.choice(string.ascii_letters + string.digits) for i in range(30)])
 
 	def comment(self):
-		return randstring()
+		return self.randstring()
 	
 	def tag_length(self):
 		return 10
 
 	def tag(self):
-		return randstring()
+		return self.randstring()
 
 	def magicalNumber(self):
 		return str(random.randint(100000, 999999))
