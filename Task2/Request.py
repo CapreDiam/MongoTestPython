a69adc1f34e3f66cf688ff0ec6add4a0e2c21f24
Dima <moggieggwp@mail.ru>
2016-01-22 20:01:49 +0200

    Update Request.py

--- Task2/Request.py
+++ Task2/Request.py
@@ -5,14 +5,17 @@ fxopen=FXOpenOrder.FXOpenOrder()
 sereliaz=Serialization.Serialization()
 
 class request(FXCMOrder.FXCMOrder,FXOpenOrder.FXOpenOrder,Serialization.Serialization):
-    inserts=[]
- 
+    #inserts=[]
+    results_db=[]
+    
     def insert_request_generation(self):
         queryForFXCM = "db.orders.insert( { "+ "provider: "+'"'+ fxcm.provider() +'"'+ " , id: " + '"'+fxcm.id()+'"'+ " , type: " +'"'+ fxcm.type() + '"'+" , price: " + fxcm.price() + " , direction: " +'"'+ fxcm.direction() +'"'+ " , currency: " + '"'+fxcm.currency() +'"'+ ", decsription: " + '"'+fxcm.decsription()+'" ,'
         
         queryForFXOpen="db.orders.insert( { "+ "provider: "+ '"'+fxopen.provider() +'"'+ " , id: " +'"'+ fxopen.id()+'"'+ " , type: " +'"'+ fxopen.type() +'"'+ " , price: " + fxopen.price() + " , direction: " +'"'+ fxopen.direction() + '"'+" , currency: " +'"'+ fxopen.currency() + '"'+" , duration: " + '"'+fxopen.duration() +'"'+ " ,  comment_length: " +'"'+ fxopen.comment_length()+'"'+ " , comment: " +'"'+ fxopen.comment() +'"'+ " , tag_length: " +'"'+ fxopen.tag_length() +'"'+ " , tag: " +'"'+fxopen.tag() +'"'+ " , magic_number: " + '"'+fxopen.magicalNumber()+'" ,'
         a=[queryForFXCM, queryForFXOpen]
-        return request().status(queryForFXOpen)
+        b=random.randint(0,1)
+        c=a[b]
+        return request().status(c)
         
         
     def status(self,string):
@@ -23,14 +26,50 @@ class request(FXCMOrder.FXCMOrder,FXOpenOrder.FXOpenOrder,Serialization.Serializ
             dt=fxcm.date_time()
             queryForDB=string+" date: new Date(" + str(dt)+')' + " , status: " + '"' + status[a][x] + '"' + ' }' + ' )' 
             operation = '"' +"mongo < "+queryForDB+'"'
-            print(operation)
-            result = commands.getoutput(operation)                
+            #print(queryForDB)
+            commands.getoutput(operation)      
+               
         
-   
+    def request_db(self):
+        request().id_by_status()
+        request().sum_by_fxcm()   
+        request().sum_by_fxopen()
+        request().status_by_id()
+            
+    def id_by_status(self):
+        statuses = ["New", "To Provider", "Partially Filled","Filled","Rejected"]
+        for i in range(len(statuses)):
+            query1= "db.orders.find( { status: "+'"'+statuses[i]+'"'+ " } ).count()"
+            operation = '"' +"mongo < "+query1+'"'
+            request.results_db.append(commands.getoutput(operation))  
+            
+    def sum_by_fxcm(self):
+        query2="db.orders.aggregate( [ { $match : { provider: "+'"'+'*'+'"'+" } }, { $group: { _id: "+'"'+"$provider"+'"'+", sum: { $sum: "+'"'+"$price"+'"'+" } } } ] )" 
+        operation = '"' +"mongo < "+query2+'"'
+        request.results_db.append(commands.getoutput(operation))
+            
+    def sum_by_fxopen(self):
+        query2="db.orders.aggregate( [ { $match : { provider: "+'"'+'~'+'"'+" } }, { $group: { _id: "+'"'+"$provider"+'"'+", sum: { $sum: "+'"'+"$price"+'"'+" } } } ] )"
+        operation = '"' +"mongo < "+query2+'"'
+        request.results_db.append(commands.getoutput(operation))
+    
+    def status_by_id(self):
+        query3="db.orders.aggregate( { $group: { _id: "+'"'+"$id"+'"'+", count: { $sum: 1 } } }, { $limit: 3 } )"
+        operation = '"' +"mongo < "+query3+'"'
+        request.results_db.append(commands.getoutput(operation))    
         
+    def between_dates(self):
+        query4="db.orders.aggregate( [ { $match : { date: { $gte: new Date("+'"'+"2016"+'\\'+"1"+'\\'+"15 9:12:24.25"+'"'+"),$lte: new Date("+'"'+"2016"+'\\'+"1"+'\\'+"15 9:12:24.725"+'"'+ ") } } }, { $group: { _id: null, count: { $sum: 1 } } } ] )"
+        operation = '"' +"mongo < "+query4+'"'
+        request.results_db.append(commands.getoutput(operation))     
+    
+    def insert_request(self):
+        a=request()
+        b=0
+        while b < 3000:
+             a.insert_request_generation()
+             b=b+1           
 
-a=request()
-b=0
-while b < 3000:
-    a.insert_request_generation()
-    b=b+1
+mongo=request()
+mongo.insert_request()
+mongo.request_db()
