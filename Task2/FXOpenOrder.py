d2e610da3ca14ea92b68530586661d75fca866d0
CapreDiam <ig.bukin2022@gmail.com>
2016-01-23 22:07:17 +0200

    Update FXOpenOrder.py

--- Task2/FXOpenOrder.py
+++ Task2/FXOpenOrder.py
@@ -1,28 +1,32 @@
-import string,random,Orders
+import string, random
 
-class FXOpenOrder(Orders.Orders):
-	
-	def __init__(self):
-		self.durations = ['Immediate or cancel', 'Good Till Cancel']
-        
-	def provider(self):
-		return '~'
-	def duration(self):
-		return self.durations[random.randint(0,1)]
-	def comment_length(self):
-		return '30'
-	
-	def __randstring(self):
-    		return ''.join([random.choice(string.ascii_letters + string.digits) for i in range(30)])
+from Orders import Orders
 
-	def comment(self):
-		return "comment"
-	
-	def tag_length(self):
-		return '10'
 
-	def tag(self):
-		return "tag"
+class FXOpenOrder(Orders):
+    def __init__(self):
+        self.durations = ['Immediate or cancel', 'Good Till Cancel']
 
-	def magicalNumber(self):
-		return str(random.randint(100000, 999999))
+    def provider(self):
+        return '~'
+
+    def duration(self):
+        return self.durations[random.randint(0, 1)]
+
+    def comment_length(self):
+        return "30"
+
+    def __randstring(self):
+        return ''.join([random.choice(string.ascii_letters + string.digits) for i in range(30)])
+
+    def comment(self):
+        return self.__randstring()
+
+    def tag_length(self):
+        return "10"
+
+    def tag(self):
+        return self.__randstring()
+
+    def magicalNumber(self):
+        return "100000000"
