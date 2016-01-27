50d559c70dc76a7fae281219185dc5c25931ddf8
Dima <moggieggwp@mail.ru>
2016-01-27 20:49:13 +0200

    Create Order.py

---
+++ Task3/Order.py
@@ -0,0 +1,66 @@
+from abc import ABCMeta, abstractmethod
+class Order:
+    __metaclass__=ABCMeta
+        
+    @abstractmethod
+    def get_id(self):
+        pass
+        
+    @abstractmethod
+    def set_id(self,id):
+        pass
+        
+    @abstractmethod
+    def get_type(self):
+        pass
+        
+    @abstractmethod
+    def set_type(self,type):
+        pass
+    
+    @abstractmethod
+    def get_price(self):
+        pass
+        
+    @abstractmethod
+    def set_price(self,price):
+        pass
+    @abstractmethod
+    def get_direction(self):
+        pass
+        
+    @abstractmethod
+    def set_direction(self,direction):
+        pass
+    
+    @abstractmethod
+    def get_currency(self):
+        pass
+        
+    @abstractmethod
+    def set_currency(self,currency):
+        pass
+        
+    @abstractmethod
+    def get_date_time(self):
+        pass
+        
+    @abstractmethod
+    def set_date_time(self,date_time):
+        pass
+        
+    @abstractmethod
+    def get_status(self):
+        pass
+        
+    @abstractmethod
+    def set_status(self,status):
+        pass
+    
+    @abstractmethod
+    def get_provider(self):
+        pass
+        
+    @abstractmethod
+    def set_provider(self,provider):
+        pass
