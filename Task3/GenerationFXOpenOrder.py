2a6edab2a092d9c4df8f37ae220d0a549af9fb23
Dima <moggieggwp@mail.ru>
2016-01-27 23:26:31 +0200

    Create GenerationFXOpenOrder.py

---
+++ Task3/GenerationFXOpenOrder.py
@@ -0,0 +1,32 @@
+import string, random
+
+from GenerationOrder import GenerationOrder
+
+
+class GenerationFXOpenOrder(GenerationOrder):
+    def __init__(self):
+        self.durations = ['Immediate or cancel', 'Good Till Cancel']
+
+    def provider(self):
+        return '~'
+
+    def duration(self):
+        return self.durations[random.randint(0, 1)]
+
+    def comment_length(self):
+        return "30"
+
+    def __randstring(self):
+        return ''.join([random.choice(string.ascii_letters + string.digits) for i in range(30)])
+
+    def comment(self):
+        return self.__randstring()
+
+    def tag_length(self):
+        return "10"
+
+    def tag(self):
+        return self.__randstring()
+
+    def magicalNumber(self):
+        return "100000000"
