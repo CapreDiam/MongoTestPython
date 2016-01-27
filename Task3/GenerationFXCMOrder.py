478c49d39606804d9a5d36c279f2963b2c7584a6
Dima <moggieggwp@mail.ru>
2016-01-27 23:34:04 +0200

    Delete GenerationFXCMOrder.py

--- Task3/GenerationFXCMOrder.py
+++
@@ -1,9 +0,0 @@
-from GenerationDataOrder import GenerationDataOrder
-
-class GenerationFXCMOrder(GenerationDataOrder):
-		
-	def provider(self):
-		return '*'
-	
-	def description(self):
-		return 'description'
