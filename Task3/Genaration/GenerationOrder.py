a7d0e63b0cd43fdb5efc26f9ecc9c5ae9da4f1de
CapreDiam <ig.bukin2022@gmail.com>
2016-01-28 02:11:49 +0200

    Update GenerationOrder.py

--- Task3/Genaration/GenerationOrder.py
+++ Task3/Genaration/GenerationOrder.py
@@ -1,35 +1,32 @@
-import random
-import uuid
+from abc import ABCMeta, abstractmethod
 
 
 class GenerationOrder:
-
     def __init__(self):
         pass
 
-    currencys = ['AUD/CHF', 'AUD/JPY', 'AUD/NZD', 'AUD/JPY', 'AUD/JPY', 'AUD/JPY', 'AUD/JPY', 'CAD/JPY', 'CHF/JPY',
-                     'EUR/AUD', 'EUR/CAD', 'EUR/CHF', 'EUR/GBP', 'EUR/JPY', 'EUR/NZD', 'GBP/AUD', 'GBP/CHF', 'GBP/JPY',
-                     'NZD/JPY', 'EUR/USD', 'GBP/USD', 'AUD/USD', 'NZD/USD', 'USD/JPY', 'USD/CHF', 'USD/CAD']
+    __metaclass__ = ABCMeta
 
+    @abstractmethod
     def provider_generated(self):
-        return '+'
+        pass
 
+    @abstractmethod
     def type_generated(self):
-        return 'market'
+        pass
 
+    @abstractmethod
     def direction_generated(self):
-        directions = ['sell', 'buy']
-        direction = directions[random.randint(0, 1)]
-        return direction
+        pass
 
+    @abstractmethod
     def id_generated(self):
-        id = str(uuid.uuid4())
-        id = id[0:7] + id[24:32]
-        return id
+        pass
 
+    @abstractmethod
     def price_generated(self):
-        price = str(random.randint(100000, 999999) + random.random())
-        return price
+        pass
 
+    @abstractmethod
     def currency_generated(self):
-        return self.currencys[random.randint(0, 21)]
+        pass
