7f0d1892423dea4ac76bb38ce50c2e94cd2db4ea
CapreDiam <ig.bukin2022@gmail.com>
2016-01-29 00:07:06 +0200

    Update Order.py

--- Task3/Data/Order.py
+++ Task3/Data/Order.py
@@ -1,66 +1,75 @@
 from abc import ABCMeta, abstractmethod
+
+
 class Order:
-    __metaclass__=ABCMeta
-        
+    def __init__(self):
+        pass
+
+    __metaclass__ = ABCMeta
+
     @abstractmethod
     def get_id(self):
         pass
-        
+
     @abstractmethod
-    def set_id(self,id):
+    def set_id(self, id):
         pass
-        
+
     @abstractmethod
     def get_type(self):
         pass
-        
+
     @abstractmethod
-    def set_type(self,type):
+    def set_type(self, type):
         pass
-    
+
     @abstractmethod
     def get_price(self):
         pass
-        
+
     @abstractmethod
-    def set_price(self,price):
+    def set_price(self, price):
         pass
+
     @abstractmethod
     def get_direction(self):
         pass
-        
+
     @abstractmethod
-    def set_direction(self,direction):
+    def set_direction(self, direction):
         pass
-    
+
     @abstractmethod
     def get_currency(self):
         pass
-        
+
     @abstractmethod
-    def set_currency(self,currency):
+    def set_currency(self, currency):
         pass
-        
+
     @abstractmethod
     def get_date_time(self):
         pass
-        
+
     @abstractmethod
-    def set_date_time(self,date_time):
+    def set_date_time(self, date_time):
         pass
-        
+
     @abstractmethod
     def get_status(self):
         pass
-        
+
     @abstractmethod
-    def set_status(self,status):
+    def set_status(self, status):
         pass
-    
+
     @abstractmethod
     def get_provider(self):
         pass
-        
+
     @abstractmethod
-    def set_provider(self,provider):
+    def set_provider(self, provider):
+        pass
+    @abstractmethod
+    def get_insert_request(self):
         pass
