c5aa6d90afe8b65207ea233c7b46f6b3a2ae534c
Dima <moggieggwp@mail.ru>
2016-01-30 00:36:02 +0200

    Update SumBetweenDate.py

--- Task3/Requests/SumBetweenDate.py
+++ Task3/Requests/SumBetweenDate.py
@@ -1,17 +1,20 @@
 from PerfomanceRequest import PerfomanceRequest
-
+import re
 class SumBetweenDate(PerfomanceRequest):
     
-    a=PerfomanceRequest()
+    
     
     def get_result_request(self):
         return self.result_request
         
     def perfomance_request(self):
-        string_result="db.orders.aggregate( [ { $match : { date: { $gte: new Date(" + '"' + "2016" + '-' + "1" + '-' + "15 9:12:24.25" \
-                        + '"' + "),$lte: new Date(" + '"' + "2016" + '-' + "1" + '-' + "15 9:12:24.725" + '"' + ") } } }, { $group: { _id:" \
-                                                                                                                " null, count: { $sum: 1 } } } ] )"
-        self.result_request.append([(self.insert_request(string_result)).split()[13]])
+        string_result="db.orders.aggregate( [ { $match : { date: { $gte: new Date(" + '"' + "2016" + '-' + "1" + '-' + "15 9:12:24.25" + '"' + "),$lte: new Date(" + '"' + "2016" + '-' + "1" + '-' + "15 9:12:24.725" + '"' + ") } } }, { $group: { _id:" " null, count: { $sum: 1 } } } ] )"
+        res=self.insert_request(string_result)
+        self.prepare_result(res)                                                                                    
+        
     
-    def prepare_result(self):
-        pass
+    def prepare_result(self,res):
+        result=re.findall(r'\n\{.+\}',res)
+        results=result[0]
+        
+        self.result_request.append(results)
