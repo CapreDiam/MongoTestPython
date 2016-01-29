bf2656b212cd2c8630888d4d0f7914d63c53584a
Dima <moggieggwp@mail.ru>
2016-01-30 00:35:08 +0200

    Update CountIdStatuses.py

--- Task3/Requests/CountIdStatuses.py
+++ Task3/Requests/CountIdStatuses.py
@@ -1,15 +1,23 @@
 from PerfomanceRequest import PerfomanceRequest
+import re
 
 class CountIdStatuses(PerfomanceRequest):
     
-    
+    string_insert = "db.orders.aggregate( { $group: { _id: "+'"'+"$id"+'"'+", count: { $sum: 1 } } }, { $limit: 3 } )"
     
     def get_result_request(self):
         return self.result_request
         
     def perfomance_request(self):
-        string_insert = "db.orders.aggregate( { $group: { _id: " + '"' + "$id" + '"' + ", count: { $sum: 1 } } }, { $limit: 3 } )"
-        self.result_request.append(((self.insert_request(string_insert)).split())[7])
+        
+        res=self.insert_request(self.string_insert)
+        self.prepare_result(res)                                                                                
+        
     
-    def prepare_result(self):
-        pass
+    def prepare_result(self,res):
+        result=re.findall(r'[1-9]+\n',res)
+        
+        for i in range(len(result)-2):
+            results=result[i+1]
+            
+            self.result_request.append(results)
