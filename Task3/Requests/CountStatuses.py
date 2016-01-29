0c8b401a0249704f538e1a903b1b59a311e9e43a
Dima <moggieggwp@mail.ru>
2016-01-30 00:35:36 +0200

    Update CountStatuses.py

--- Task3/Requests/CountStatuses.py
+++ Task3/Requests/CountStatuses.py
@@ -1,17 +1,23 @@
 from PerfomanceRequest import PerfomanceRequest
+import re
 
 class CountStatuses(PerfomanceRequest):
     
-    
+    statuses = ["New", "To Provider", "Partially Filled", "Filled", "Rejected"]
     
     def get_result_request(self):
         return self.result_request
         
     def perfomance_request(self):
-        statuses = ["New", "To Provider", "Partially Filled", "Filled", "Rejected"]
-        for i in range(len(statuses)):
-            string_insert = "db.orders.find( { status: " + '"' + statuses[i] + '"' + " } ).count()"
-            self.result_request.append(((self.insert_request(string_insert)).split())[7])
+        
+        for i in range(len(self.statuses)):
+            string_insert = "db.orders.find( { status: " + '"' + self.statuses[i] + '"' + " } ).count()"
+            res=self.insert_request(string_insert)
+            self.prepare_result(res)
+            
     
-    def prepare_result(self):
-        pass
+    def prepare_result(self,res):
+        result=re.findall(r'\n[1-9]+',res)
+        result=result[0]
+                                                                                           
+        self.result_request.append(result)
